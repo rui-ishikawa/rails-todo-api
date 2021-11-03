e48e6fb8e1bc614451027d98a1bcba22d36c7186
rui.ishikawa <rui.ishikawa.business@gmail.com>
2021-11-03 16:50:49 +0900

    crud作成

--- app/controllers/api/v1/todo_controller.rb
+++ app/controllers/api/v1/todo_controller.rb
@@ -1,5 +1,44 @@
 class Api::V1::TodoController < ApplicationController
+    before_action :set_todo, only: %i[update, destroy]
+
     def index
-        render json: {status: 200, message: "Hello world?"}
+        todos = Todo.all
+        render json: {status: 200, todos: todos}
+    end
+
+    def create 
+        todo = Todo.new(todo_params)
+        if todo.save
+            render json: {status: 200, todo: todo}
+        else
+            render json: {status:500}
+        end
+    end
+
+    def update
+        @todo.title = todo_params[:title]
+        @todo.description = todo_params[:description]
+        @todo.status = todo_params[:status]
+        if @todo.save
+            render json: {status:200, todo: @todo}
+        else
+            render json: {status:500}
+        end
+    end
+
+    def destroy
+        @todo.destroy
+        render json: {status: 'SUCCESS'}
+    end
+
+
+    private
+
+    def set_todo
+        @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+        params.permit(:title, :description, :status)
     end
 end
--- db/migrate/20211103063111_create_todos.rb
+++ db/migrate/20211103063111_create_todos.rb
@@ -1,7 +1,9 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
-
+      t.string :title, null: false
+      t.string :description, null: false
+      t.string :status, null: false
       t.timestamps
     end
   end
